8268ab3af7493fa518a6ad9b610ec5ec867da03b
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2023-02-19 02:34:39 -0500

    Step 8 Complete

---
+++ lab-tunley/AlbumCell.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCell.swift
+//  lab-tunley
+//
+//  Created by Paul Leiva on 2/19/23.
+//
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+}
